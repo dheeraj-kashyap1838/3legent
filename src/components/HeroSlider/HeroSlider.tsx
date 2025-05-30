9e0f5dd7d57ba707022b37ab4b5ee3c1fe1ee1b1
dheeraj-kashyap1838 <dheeraj.kashyap1838@gmail.com>
2025-05-30 15:29:13 +0530

    feat: add HeroSlider component and configuration for Slice Machine

--- src/components/HeroSlider/HeroSlider.tsx
+++ src/components/HeroSlider/HeroSlider.tsx
@@ -10,36 +10,40 @@ import { PrismicImage, PrismicRichText } from "@prismicio/react";
 interface HeroSlidesProps {
   slice: Content.HeroSliderSlice;
 }
-
+import * as prismic from "@prismicio/client";
 export default function HeroSlider({ slice }: HeroSlidesProps) {
   const { description, heading, sliderImage } = slice?.primary;
   return (
     <section className="px-6" id="hero_slider">
       <div className="container mx-auto">
-        <Swiper
-          navigation={true}
-          pagination={true}
-          modules={[Navigation, Pagination]}
-          className="mySwiper "
-        >{
-          sliderImage && 
-          sliderImage?.map((elem, id) => {
-            return (
-              <SwiperSlide key={id}>
-                <div className="max-h-[536px] h-full flex justify-center items-end">
-                  <PrismicImage field={elem?.image} />
-                </div>
-              </SwiperSlide>
-            );
-          })
-        }
-        </Swiper>
+        {prismic.isFilled.group(sliderImage) && (
+          <Swiper
+            navigation={true}
+            pagination={true}
+            modules={[Navigation, Pagination]}
+            className="mySwiper "
+          >
+            {sliderImage?.map((elem, id) => {
+              return (
+                <SwiperSlide key={id}>
+                  <div className="max-h-[536px] h-full flex justify-center items-end">
+                    <PrismicImage field={elem?.image} />
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        )}
         <div className="flex md:flex-row flex-col gap-4 items-center justify-center pt-8 pb-10">
           <div className=" max-w-[643px] w-full font_poppins font-medium lg:text-7xl text-[40px] md:leading-[76px] leading-[44px]">
-            <PrismicRichText field={heading} />
+            {prismic.isFilled.richText(heading) && (
+              <PrismicRichText field={heading} />
+            )}
           </div>
           <div className="flex font_inter font-semibold text_primary  max-w-[477px] w-full">
-            <PrismicRichText field={description} />
+            {prismic.isFilled.richText(heading) && (
+              <PrismicRichText field={description} />
+            )}
           </div>
         </div>
       </div>
